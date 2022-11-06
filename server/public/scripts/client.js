93873cfc2678178ad51ac0c405ee2124053eca88
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-11-06 14:26:17 -0600

    fixing up my calculator

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,7 @@ $(document).ready(onReady);
 function onReady() {
     console.log('js ready!');
     //have past entries stay on DOM
-    //getAnswers();
+    getAnswers();
     //click listeners
     $('#equalButton').on('click', makeNumberObject);
     $('#plusButton').on('click', addPlus);
@@ -14,3 +14,83 @@ function onReady() {
     $('#clearButton').on('click', clearInputs);
     $('#resetButton').on('click', clearDom);
 }
+
+let operator;
+
+//post requests
+
+
+//making the object to send to the server
+function makeNumberObject() {
+    console.log('clicked equals')
+    firstNumber: Number($('#firstNumber').val()),
+        secondNumber; Number($('#secondNumber').val()),
+        operator: operator
+
+    }
+    console.log(calcsToDo);
+    //post inputs to server
+
+$.ajax({
+    method: 'POST',
+    url: '/calculator',
+    data: calcsToDo
+}).then(function (response) {
+    console.log(response);
+    getAnswers();
+})
+    //empty input
+    $('input').val('');
+
+
+//get requests
+
+function getAnswers() {
+    $.ajax({
+        url: '/calculator',
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        $('#answerHistory').append(`<li>${response}</li>`);
+
+    })
+
+}
+
+// operator functions
+function addPlus() {
+    let operatorToPost = "+";
+    operator = operatorToPost;
+}
+
+function addMinus() {
+    let operatorToPost = "-";
+    operator = operatorToPost;
+}
+
+function addMultiply() {
+    let operatorToPost = "*";
+    operator = operatorToPost;
+}
+
+function addDivide() {
+    let operatorToPost = "/";
+    operator = operatorToPost;
+}
+
+
+function clearInputs() {
+    $('input').val('');
+}
+
+function clearDom() {
+    console.log('yessir');
+    $.ajax({
+        method: 'GET',
+        url: '/clearDom'
+    }).then(function (response) {
+        operator = '';
+        $('#answerHistory').empty();
+        $('#answerHistory').append(response);
+    })
+};
---
+++ server/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000;
+
+//store these in an array to keep the history
+let equationString = [];
+let equation;
+
+
+// get body parser started
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+//post requests
+app.post('/calculator', (req, res) => {
+    let equation = req.body;
+    let firstNum = equation.firstNumber;
+    let secondNum = equation.secondNumber;
+    let operator = equation.operator;
+
+    if (operator === "+") {
+        let outcome = Number(firstNum) + Number(secondNum);
+        equation = `${firstNum}${operator}${secondNum} = ${outcome}`;
+        equationString.push(equation);
+    }
+    else if (operator === "-") {
+        let outcome = Number(firstNum) - Number(secondNum);
+        equation = `${firstNum}${operator}${secondNum} = ${outcome}`;
+        equationString.push(equation);
+    }
+    else if (operator === "*") {
+        let outcome = Number(firstNum) * Number(secondNum);
+        equation = `${firstNum}${operator}${secondNum} = ${outcome}`;
+        equationString.push(equation);
+    }
+    else if (operator === "/") {
+        let outcome = Number(firstNum) / Number(secondNum);
+        equation = `${firstNum}${operator}${secondNum} = ${outcome}`;
+        equationString.push(equation);
+    }
+    res.sendStatus(201);
+})
+
+app.get('/calculator', function (req, res) {
+    console.log('in get function');
+    res.send(equationString);
+    equationString = [];
+})
+
+
+app.listen(PORT, () => {
+    console.log('server is running on', PORT)
+});
+
+
+//GET EMPTY ARRAY
+app.get('/clearDom', function (req, res) {
+    console.log('in clearDom');
+    equationString = [];
+    res.send(equationString);
+})
+
